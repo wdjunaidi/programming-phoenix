76af296cbd4a2a4169b64eabf35c656ef58e48ea
Wirianto Djunaidi <wirianto.djunaidi@gmail.com>
2018-05-29 06:22:08 +1200

    Moving authentication plug into function plug

--- rumbl/web/controllers/user_controller.ex
+++ rumbl/web/controllers/user_controller.ex
@@ -1,40 +1,38 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
+  plug(:authenticate when action in [:index, :show])
   alias Rumbl.User
 
   def index(conn, _params) do
-    case authenticate(conn) do
-      %Plug.Conn{halted: true} = conn ->
-        conn
-      conn ->
-        users = Repo.all(User)
-        render conn, "index.html", users: users
-    end
+    users = Repo.all(User)
+    render(conn, "index.html", users: users)
   end
 
   def show(conn, %{"id" => id}) do
     user = Repo.get(User, id)
-    render conn, "show.html", user: user
+    render(conn, "show.html", user: user)
   end
 
   def new(conn, _params) do
     changeset = User.changeset(%User{})
-    render conn, "new.html", changeset: changeset
+    render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"user" => user_params}) do
     changeset = User.registration_changeset(%User{}, user_params)
+
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: user_path(conn, :index))
+
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
   end
 
-  def authenticate(conn) do
+  def authenticate(conn, _opts) do
     if conn.assigns.current_user do
       conn
     else
